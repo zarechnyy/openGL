24ef38b1d809cf1e18c23641effcf2fc226dab06
Yaroslav Zarichnyi <yarik120700@gmail.com>
2019-09-07 20:14:29 +0300

    Initial Commit

---
+++ laba2_graphics/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  laba2_graphics
+//
+//  Created by Yaroslav Zarechnyy on 9/7/19.
+//  Copyright © 2019 Yaroslav Zarechnyy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
